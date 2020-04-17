d33daa64807e945f3a65b83c74ceeaedb2209125
Kalpesh <patelkalps@gmail.com>
2020-04-17 17:39:13 -0400

    branch 1 commit

--- server.js
+++ server.js
@@ -1,10 +1,1 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-    res.send("Hello from server.js")
-});
-
-app.listen(3000);
-
-console.log("Server running on port 3000");
+console.log("Hello from KP! His first app");
